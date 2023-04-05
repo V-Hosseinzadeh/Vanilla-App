a8920aa7b5b671c83ffa8020f28075bf15d86013
V-Hosseinzadeh <129286553+V-Hosseinzadeh@users.noreply.github.com>
2023-04-05 23:48:17 +0430

    added some js

--- src/app.js
+++ src/app.js
@@ -1,2 +1,70 @@
-let apiKey= "b80ba723209860a35452o2bb8f4f0tb3";
-let apiUrl= `https://api.shecodes.io/weather/v1/current?query={query}&key=${apiKey}`;
+function formatDate(timestamp) {
+    let date= new Date(timestamp);
+    let hours= date.getHours();
+    if (hours < 10) {
+        hours = `0${hours}`;
+    }
+
+    let minutes= date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+
+    let days= [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ];
+
+    let day = days[date.getDay()];
+    return`${day} ${hours}:${minutes}`;
+}
+
+function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  
+    return days[day];
+  }
+
+
+function showTemperature (response) {
+let temperatureElement= document.querySelector("#temperature");
+let cityElement= document.querySelector("#city");
+let descriptionElement= document.querySelector("#description");
+let humidityElement= document.querySelector("#humidity");
+let windElement= document.querySelector("#wind");
+let dateElement= document.querySelector("#date");
+let iconElement= document.querySelector("#icon");
+
+temperatureElement.innerHTML= Math.round(response.data.temperature.current);
+cityElement.innerHTML= response.data.city;
+descriptionElement.innerHTML= response.data.condition.description;
+humidityElement.innerHTML= response.data.temperature.humidity;
+windElement.innerHTML= Math.round(response.data.wind.speed);
+dateElement.innerHTML= formatDate(response.data.time * 1000);
+iconElement.setAttribute ("src", `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`);
+iconElement.setAttribute("alt", response.data.condition.description);
+}
+
+function search(city) {
+    let apiKey= "b80ba723209860a35452o2bb8f4f0tb3";
+    let apiUrl= `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(showTemperature);
+    }
+    
+
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+search ("Tehran");
